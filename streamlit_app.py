0a701c5716bdeaf8938d420a1a2739be99e359f2
blair young <byhillsboro@gmail.com>
2021-12-31 13:10:46 +0000

    ability to add entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from google.oauth2 import service_account
 import gspread
 import pandas as pd
+from datetime import datetime
+
 
 key_dict = json.loads(st.secrets["textkey"])
 
@@ -24,11 +26,15 @@ def search(database, criteria=False):
 
 def search_output(search_result, criteria=False):
  	if not criteria:
- 		return search_result.iloc[0]['Name']
+ 		return search_result.iloc[0]['name']
 
-def add_place(name, df, criteria=False):
-	if not criteria:
-		sheet.append_rows(values=[[len(df)+1, str(name)]])
+def add_place(df, entry_criteria):
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	db_logging = [len(df)+1, current_time]
+	db_entry = db_logging + entry_criteria
+	print(db_entry)	
+	sheet.append_rows(values=[db_entry])
 		
 
 
@@ -38,16 +44,55 @@ suggestion_cta = st.button('Get suggestion')
 
 if suggestion_cta:
 	suggested_output = search_output(search(df))
+	st.success(f'let\'s go to {suggested_output}!')
 	st.write(suggested_output)
 	st.balloons()
 
 
-name = st.text_input("Or add a new place place below")
+st.text('Add a place')
+with st.form(key='new_place_form', clear_on_submit=True):
+	place_name = st.text_input("Place name")
 
-if st.button('Submit'):
-	result = name.title()
-	add_place(result, df)
-	st.success(f'{result} has been added!')
+	st.text('Meal type')
+	breakfast_option, brunch_option, lunch_option, dinner_option, roast_option, drinks_option = st.columns(6)
+	with breakfast_option:
+		breakfast = st.checkbox('Breakfast')
+	with brunch_option:
+		brunch = st.checkbox('Brunch')
+	with lunch_option:
+		lunch = st.checkbox('Lunch')
+	with dinner_option:
+		dinner = st.checkbox('Dinner')
+	with roast_option:
+		roast = st.checkbox('Roast')
+	with drinks_option:
+		drinks = st.checkbox('Drinks')
+
+	st.text('Venue type')
+	restaurant_option, cafe_option, bar_option, pub_option = st.columns(4)
+	with restaurant_option:
+		restaurant = st.checkbox('Restaurant')
+	with cafe_option:
+		cafe = st.checkbox('Cafe')
+	with bar_option:
+		bar = st.checkbox('Bar')
+	with pub_option:
+		pub = st.checkbox('Pub')
+
+	added_by = st.radio("Added by", ('Blair', 'Grace'))
+
+	anything_else = st.text_input("Anything else?")
+
+	submit_button = st.form_submit_button(label='Submit')
+
+entry_criteria = [place_name, breakfast, brunch, lunch,
+				  dinner, roast, drinks, restaurant,
+				  cafe, bar, pub, added_by, anything_else]
+
+
+if submit_button:
+    add_place(df, entry_criteria)
+    st.success(f'{place_name} has been added!')
 
 
 
@@ -67,25 +112,3 @@ if st.button('Submit'):
 
 
 
-
-# if add_place_cta:
-# 	name = st.text_input("label goes here")
-# 	if(st.button('Submit')):
-# 		result = name.title()
-# 		st.success(result)
-# 		print(result)
-
-
- 
-# display the name when the submit button is clicked
-# .title() is used to get the input text string
-# if(st.button('Submit')):
-#     result = name.title()
-#     st.success(result)
-
-# if add_place_cta:
-# 	place_name = st.text_area("label goes here")
-# 	print('ddd')
-# 	print(place_name.title())
-# 	print(type(place_name))
-# 	add_place(place_name, df)
