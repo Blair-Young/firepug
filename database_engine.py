2f3dd53bf44586479f338cec11a96013566e583a
blair young <byhillsboro@gmail.com>
2022-01-23 16:41:31 +0000

    upgrade from google sheets to postgres + refactor

---
+++ database_engine.py
@@ -0,0 +1,110 @@
+import psycopg2
+import json
+import streamlit as st
+from google.oauth2 import service_account
+import gspread
+import pandasql as ps
+import pandas as pd
+from datetime import datetime
+import uuid
+
+
+class Database():
+	def __init__(self):
+		self.connection = psycopg2.connect(host= st.secrets["host"],
+										database=st.secrets["database"],
+										user=st.secrets["user"],
+										password=st.secrets["password"])
+
+	def search(self, query):
+		cur = self.connection.cursor()
+		cur.execute(query)
+		result = cur.fetchall()
+		cur.close()
+		self.connection.close()
+		return self.format_results(result)
+
+	def format_results(self, result):
+		if not len(result):
+			return 'No entries with this criteria'
+		return Location(result[0]).name
+
+	def generate_query(self, meal_criteria=None, venue_criteria=None):
+		if meal_criteria=='Don\'t mind' and venue_criteria=='Don\'t mind':
+			return """SELECT * FROM locations ORDER BY RANDOM()"""
+		
+		if meal_criteria=='Don\'t mind' and venue_criteria!='Don\'t mind':
+			venue_criteria_filter = f'venue_type_{venue_criteria}'.lower()
+			return f"""SELECT * FROM locations WHERE {venue_criteria_filter}=True ORDER BY RANDOM() LIMIT 1"""
+
+		if venue_criteria=='Don\'t mind' and meal_criteria!='Don\'t mind':
+			meal_criteria_filter = f'meal_type_{meal_criteria}'.lower()
+			return f"""SELECT * FROM locations WHERE {meal_criteria_filter}=True ORDER BY  RANDOM() LIMIT 1"""
+		else:
+			meal_criteria_filter = f'meal_type_{meal_criteria}'.lower()
+			venue_criteria_filter = f'venue_type_{venue_criteria}'.lower()
+			return f"""SELECT * FROM locations WHERE {meal_criteria_filter}=True AND {venue_criteria_filter}=True ORDER BY  RANDOM() LIMIT 1"""
+
+
+	def add_place(self, entry_criteria):
+		now = datetime.now()
+		current_time = now.strftime("%d/%m/%Y %H:%M:%S")
+		uid = str(uuid.uuid4())
+		db_logging = [uid, current_time]
+		location_attributes = db_logging + entry_criteria
+		print('loc att', location_attributes)
+		db_entry = Location(location_attributes)
+		command = f"""SET DateStyle TO European;
+						INSERT INTO locations VALUES(
+													'{db_entry.uid}', 
+													'{db_entry.date_added}',
+													'{db_entry.name}',
+													{db_entry.meal_type_breakfast},
+													{db_entry.meal_type_brunch},
+													{db_entry.meal_type_lunch},
+													{db_entry.meal_type_dinner},
+													{db_entry.meal_type_roast},
+													{db_entry.meal_type_drinks},
+													{db_entry.venue_type_restaurant},
+													{db_entry.venue_type_cafe},
+													{db_entry.venue_type_bar},
+													{db_entry.venue_type_pub},
+													'{db_entry.added_by}',
+													'{db_entry.anything_else}')
+													"""
+		print(command)
+		cur = self.connection.cursor()
+		cur.execute(command)
+		self.connection.commit()
+		cur.close()
+		self.connection.close()
+
+	def place_exists(self, place_name):
+		query = f"""SELECT * FROM locations WHERE LOWER(name) = LOWER('{place_name}')"""
+		print(query)
+		cur = self.connection.cursor()
+		cur.execute(query)
+		result = cur.fetchall()
+		print(result)
+		if len(result):
+			return True
+		return False 
+
+class Location():
+	def __init__(self, attributes):
+		self.uid = attributes[0]
+		self.date_added= attributes[1]
+		self.name = attributes[2]
+		self.meal_type_breakfast= attributes[3]
+		self.meal_type_brunch= attributes[4]
+		self.meal_type_lunch= attributes[5]
+		self.meal_type_dinner= attributes[6]
+		self.meal_type_roast= attributes[7]
+		self.meal_type_drinks= attributes[8]
+		self.venue_type_restaurant= attributes[9]
+		self.venue_type_cafe= attributes[10]
+		self.venue_type_bar= attributes[11]
+		self.venue_type_pub= attributes[12]
+		self.added_by= attributes[13]
+		self.anything_else= attributes[14]
+		
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,78 +1,10 @@
-import json
 import streamlit as st
-from google.oauth2 import service_account
-import gspread
-import pandasql as ps
 import pandas as pd
 from datetime import datetime
 import uuid
+from database_engine import Database, Location
 
-
-key_dict = json.loads(st.secrets["textkey"])
-
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-client = gspread.service_account_from_dict(key_dict)
-
-sheet = client.open('locations').get_worksheet(0)
-
-records_data = sheet.get_all_records()
-
-df = pd.DataFrame.from_dict(records_data)
-booleanDictionary = {"TRUE": True, "FALSE": False}
-df = df.replace(booleanDictionary)
-pysqldf = lambda q: ps.sqldf(q, globals())
-
-
-def format_result(result):
-    if len(result):
-        return result[0]
-    return 'No entries with this criteria'
-
-def random_search(database):
-    query = """SELECT * FROM df ORDER BY RANDOM()"""
-    return format_result(pysqldf(query)['name'].values)
-
-def suggested_search(database, meal_criteria, venue_criteria):
-
-    if meal_criteria=='Don\'t mind' and venue_criteria=='Don\'t mind':
-        return random_search(database)
-
-    if meal_criteria=='Don\'t mind' and venue_criteria!='Don\'t mind':
-        venue_criteria_filter = f'venue_type_{venue_criteria}'.lower()
-        query = f"""SELECT * FROM df WHERE {venue_criteria_filter}=True ORDER BY RANDOM() LIMIT 1"""
-        return format_result(pysqldf(query)['name'].values)
-    
-    if venue_criteria=='Don\'t mind' and meal_criteria!='Don\'t mind':
-        meal_criteria_filter = f'meal_type_{meal_criteria}'.lower()
-        query = f"""SELECT * FROM df WHERE {meal_criteria_filter}=True ORDER BY  RANDOM() LIMIT 1"""
-        return format_result(pysqldf(query)['name'].values)
-    
-    else:
-        meal_criteria_filter = f'meal_type_{meal_criteria}'.lower()
-        venue_criteria_filter = f'venue_type_{venue_criteria}'.lower()
-        query = f"""SELECT * FROM df WHERE {meal_criteria_filter}=True AND
-        {venue_criteria_filter}=True ORDER BY  RANDOM() LIMIT 1"""
-        return format_result(pysqldf(query)['name'].values)
-
-
-
-def add_place(df, entry_criteria):
-	now = datetime.now()
-	current_time = now.strftime("%d/%m/%Y %H:%M:%S")
-	uid = str(uuid.uuid4())
-	db_logging = [uid, current_time]
-	db_entry = db_logging + entry_criteria
-	sheet.append_rows(values=[db_entry])
-	
-def check_entry(database, name):
-	places = database['name'].tolist()
-	if name not in places:
-		return True
-	return False
-		
-
-
+db = Database()
 
 st.title("Hello FIRE PUG user")
 
@@ -81,8 +13,9 @@ suggestion_expander = st.expander(label='Get suggestion')
 with suggestion_expander:
 	random_suggestion_cta = st.button('Get random suggestion')
 	if random_suggestion_cta:
-		suggested_output = random_search(df)
-		st.success(f'let\'s go to {suggested_output}!')
+		query = db.generate_query('Don\'t mind', 'Don\'t mind')
+		suggested_result = db.search(query)
+		st.success(f'let\'s go to {suggested_result}!')
 		st.balloons()
 	
 	with st.form(key="suggestion_requirements_form", clear_on_submit=True):
@@ -96,7 +29,8 @@ with suggestion_expander:
 
 		suggestion_submit_button = st.form_submit_button(label='Submit')
 	if suggestion_submit_button:
-		suggested_result = suggested_search(df, meal_type, venue_type)
+		query = db.generate_query(meal_type, venue_type)
+		suggested_result = db.search(query)
 		if suggested_result != 'No entries with this criteria':
 			st.success(f'let\'s go to {suggested_result}!')
 			st.balloons()
@@ -138,7 +72,7 @@ with add_place_expander:
 
 		added_by = st.radio("Added by", ('Blair', 'Grace'))
 
-		anything_else = st.text_input("Anything else?")
+		anything_else = st.text_input("Anything else?").rstrip()
 
 		submit_button = st.form_submit_button(label='Submit')
 
@@ -148,12 +82,9 @@ with add_place_expander:
 
 
 	if submit_button:
-		if check_entry(df, place_name):
-			add_place(df, entry_criteria)
+		if not db.place_exists(place_name):
+			db.add_place(entry_criteria)
 			st.success(f'{place_name} has been added!')
 		else:
 			st.error('This place has already been added!')
-
-
-
 
